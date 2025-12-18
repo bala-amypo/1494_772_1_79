9c9a89c0981c30d23a7af8c5d7d3253a98975b50
bala-amypo <bala@amypo.in>
2025-12-18 17:49:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.VehicleEntity;
+import com.example.demo.service.VehicleService;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @PostMapping
+    public VehicleEntity addVehicle(@RequestBody VehicleEntity vehicle) {
+        return vehicleService.addVehicle(vehicle);
+    }
+
+    @GetMapping
+    public List<VehicleEntity> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+}
