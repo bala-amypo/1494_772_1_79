e90073e2c2d9b65515b4792b9ed4d36cf979c5fd
bala-amypo <bala@amypo.in>
2025-12-18 17:20:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/Shipment.java
+++ src/main/java/com/example/demo/entity/Shipment.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import jakarta.persistence.*;
 
 @Entity
-public class ShipmentEntity {
+public class Shipment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
