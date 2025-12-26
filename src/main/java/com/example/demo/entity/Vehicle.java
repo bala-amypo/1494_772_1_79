199f7a3a6a7787f1f8878de3c131dfc9d2ce2290
bala-amypo <bala@amypo.in>
2025-12-26 15:43:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -16,12 +19,17 @@ public class Vehicle {
     private Long id;
 
     @ManyToOne(optional=false)
+    @NotNull(message = "User cannot be null")
     private User user;
 
     @Column(unique=true)
+    @NotBlank(message = "Vehicle number cannot be blank")
     private String vehicleNumber;
 
+    @NotNull(message = "Capacity cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Capacity must be greater than 0")
     private Double capacityKg;
 
+    @DecimalMin(value = "0.0", inclusive = false, message = "Fuel efficiency must be greater than 0")
     private Double fuelEfficiency;
 }
