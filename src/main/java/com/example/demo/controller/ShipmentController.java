8c988d70dfb55bf5ea65e3d98ed9faa77f8b4787
bala-amypo <bala@amypo.in>
2025-12-26 04:58:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/ShipmentController.java
+++ src/main/java/com/example/demo/controller/ShipmentController.java
@@ -1,26 +1,26 @@
-
-
-
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.entity.ShipmentEntity;
+import com.example.demo.entity.Shipment;
 import com.example.demo.service.ShipmentService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shipments")
 public class ShipmentController {
 
-    private final ShipmentService shipmentService;
+    private final ShipmentService service;
 
-    public ShipmentController(ShipmentService shipmentService) {
-        this.shipmentService = shipmentService;
+    public ShipmentController(ShipmentService service){
+        this.service = service;
     }
 
-    @PostMapping
-    public ShipmentEntity createShipment(@RequestBody ShipmentEntity shipment) {
-        return shipmentService.createShipment(shipment);
+    @PostMapping("/{vehicleId}")
+    public Shipment create(@PathVariable Long vehicleId, @RequestBody Shipment s){
+        return service.createShipment(vehicleId, s);
+    }
+
+    @GetMapping("/{shipmentId}")
+    public Shipment get(@PathVariable Long shipmentId){
+        return service.getShipment(shipmentId);
     }
 }
-
